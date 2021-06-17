c01eb050f520531bff3af6cb07b12169286d21d1
Ziv <zivlazarov@gmail.com>
2021-06-17 07:26:47 +0300

    added Pair class to keep track of moves for each piece, making a pair of currentTile "to" targetTile in a stack. Made MovesLog.java a singleton.

--- src/com/zivlazarov/chessengine/logs/MovesLog.java
+++ src/com/zivlazarov/chessengine/logs/MovesLog.java
@@ -1,6 +1,27 @@
 package com.zivlazarov.chessengine.logs;
 
+import com.zivlazarov.chessengine.model.utils.Piece;
+import com.zivlazarov.chessengine.model.utils.Player;
+
+import java.util.HashMap;
+
 class MovesLog {
 
     // hashmap of <Piece, Tile> of piece moved and tile it moved to
+    final private HashMap<Player, Piece> log;
+
+    private static MovesLog instance;
+
+    private MovesLog() {
+        log = new HashMap<>();
+    }
+
+    public MovesLog getInstance() {
+        if (instance == null) instance = new MovesLog();
+        return instance;
+    }
+
+    public HashMap<Player, Piece> getLog() {
+        return log;
+    }
 }
---
+++ src/com/zivlazarov/chessengine/model/Pair.java
@@ -0,0 +1,12 @@
+package com.zivlazarov.chessengine.model;
+
+public class Pair<T, V> {
+
+    private T first;
+    private V second;
+
+    public Pair(T first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+}
