197302dcb5e2436dffa93bb38d15c78f30d43ac9
Ziv <zivlazarov@gmail.com>
2021-06-19 22:42:12 +0300

    added BoardController.java class

---
+++ src/com/zivlazarov/chessengine/controllers/BoardController.java
@@ -0,0 +1,40 @@
+package com.zivlazarov.chessengine.controllers;
+
+import com.zivlazarov.chessengine.model.utils.board.Board;
+import com.zivlazarov.chessengine.model.utils.board.Tile;
+import com.zivlazarov.chessengine.model.utils.player.Player;
+
+public class BoardController {
+
+    private Board board;
+
+    public BoardController() {}
+
+    public void printBoard() {
+        board.printBoard();
+    }
+
+    public void printBoard(Tile tileChosen) {
+        board.printBoard(tileChosen);
+    }
+
+    public void printBoardUpsideDown() {
+        board.printBoardUpsideDown();
+    }
+
+    public void printBoardUpsideDown(Tile tileChosen) {
+        board.printBoardUpsideDown(tileChosen);
+    }
+
+    public void checkBoard(Player currentPlayer) {
+        board.checkBoard(currentPlayer);
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+}
