be3c2797c8974749b01b02c227ed89a6560b09e9
Ziv <zivlazarov@gmail.com>
2021-07-04 00:26:46 +0300

    added a UI game mode

--- src/com/zivlazarov/chessengine/ui/UIGame.java
+++ src/com/zivlazarov/chessengine/ui/UIGame.java
@@ -4,22 +4,44 @@ import com.zivlazarov.chessengine.controllers.BoardController;
 import com.zivlazarov.chessengine.controllers.PlayerController;
 import com.zivlazarov.chessengine.model.board.Board;
 import com.zivlazarov.chessengine.model.board.PieceColor;
+import com.zivlazarov.chessengine.model.board.Tile;
+import com.zivlazarov.chessengine.model.board.TileColor;
+import com.zivlazarov.chessengine.model.pieces.Piece;
 import com.zivlazarov.chessengine.model.player.Player;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UIGame {
 
     JFrame frame;
     Board board;
-    Container container;
-    private JButton[][] buttons = new JButton[8][8];
+    JLayeredPane mainPanel;
+    GridLayout gridLayout;
+
+    private final Map<JButton, Tile> buttonTileMap;
+    private final Map<Tile, JButton> tileComponentMap;
+
+    private final Map<Tile, Piece> tilePieceMap;
+    private final Map<Piece, JButton> pieceButtonMap;
+
+    boolean gameStarted = false;
 
     public UIGame() {
         board = Board.getInstance();
         Player whitePlayer = new Player(board, PieceColor.WHITE);
         Player blackPlayer = new Player(board, PieceColor.BLACK);
+
+        buttonTileMap = new HashMap<>();
+        tileComponentMap = new HashMap<>();
+        tilePieceMap = new HashMap<>();
+        pieceButtonMap = new HashMap<>();
+
+        gridLayout = new GridLayout(8, 8, 0, 0);
 
         CommandLineGame.initPieces(whitePlayer, blackPlayer);
 
@@ -36,28 +58,87 @@ public class UIGame {
         int width = 800;
         int height = 600;
         frame = new JFrame("Chess");
-        container = frame.getContentPane();
-        container.setLayout(new GridLayout(8, 8, 0, 0));
+        mainPanel = new JLayeredPane();
+        mainPanel.setLayout(gridLayout);
+        mainPanel.setBounds(0, 0, 0, 0);
+        mainPanel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+        frame.add(mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        for (int row = 0; row < 8; row++) {
-            for (int col = 0; col < 8; col++) {
-                JLabel label;
-                ImageIcon icon;
-                if (board.getBoard()[row][col].getPiece() != null) {
-                    label = new JLabel();
-                    icon = new ImageIcon("/Users/zivlazarov/Projects/Java/ChessGame/src/" +
-                            board.getBoard()[row][col].getPiece().getImageName());
-                    label.setIcon(icon);
-                } else {
-                    label = new JLabel("            -");
+        frame.setResizable(false);
+
+        // alert dialog for asking if player wants to start
+        Object[] buttons = { "Yes", "No" };
+        int optionClicked = JOptionPane.showOptionDialog(null, "Would you like to start a game? ", "Chess", JOptionPane.DEFAULT_OPTION,
+                JOptionPane.QUESTION_MESSAGE, null, buttons, buttons[0]);
+        if (optionClicked != -1) {
+            // clicked yes
+            if (optionClicked == 0) gameStarted = true;
+            else if (optionClicked == 1) System.exit(1);
+        }
+
+        Map<TileColor, String> tileColorNameMap = new HashMap<>();
+        tileColorNameMap.put(TileColor.WHITE, "whiteTile.png");
+        tileColorNameMap.put(TileColor.BLACK, "blackTile.png");
+
+        for (Tile[] tiles : board.getBoard()) {
+            for (Tile tile : tiles) {
+                JButton tileButton = new JButton();
+                tileButton.setIcon(new ImageIcon(
+                        "/Users/zivlazarov/Projects/Java/ChessGame/src/" + tileColorNameMap.get(tile.getTileColor())));
+                tileButton.setBorderPainted(false);
+                tileButton.setLocation(tile.getCol(), tile.getRow());
+
+                boolean addedATileButton = false;
+
+                JButton pieceButton = null;
+                if (!tile.isEmpty()) {
+                    pieceButton = new JButton();
+                    pieceButton.setIcon(new ImageIcon("/Users/zivlazarov/Projects/Java/ChessGame/src/" + tile.getPiece().getImageName()));
+                    pieceButton.setSize(48, 48);
+                    pieceButton.setLocation(tileButton.getLocation());
+                    pieceButton.setBorderPainted(false);
+                    tile.getPiece().setImageIcon(pieceButton.getIcon());
+//                    tileButton.add(pieceButton);
+                    pieceButtonMap.put(tile.getPiece(), pieceButton);
+                    tilePieceMap.put(tile, tile.getPiece());
+
+                    mainPanel.add(tileButton, JLayeredPane.DEFAULT_LAYER);
+                    mainPanel.add(pieceButton, JLayeredPane.DRAG_LAYER);
+                    addedATileButton = true;
                 }
-                frame.add(label);
+
+                tile.setButton(tileButton);
+                buttonTileMap.put(tileButton, tile);
+                tileComponentMap.put(tile, tileButton);
+
+                if (!addedATileButton) {
+                    mainPanel.add(tileButton, JLayeredPane.DEFAULT_LAYER);
+                }
             }
         }
+
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setSize(width, height);
         frame.setVisible(true);
+
+        for (Component component : mainPanel.getComponents()) {
+            component.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    super.mouseClicked(e);
+                    JButton button = (JButton) component;
+                    Tile tile = buttonTileMap.get(button);
+                    Piece piece = tilePieceMap.get(tile);
+
+                    System.out.println(piece.getName());
+                }
+            });
+        }
+
+//        while (gameStarted) {
+//
+//        }
     }
 
     public static void main(String[] args) {
