c7138495e88ba6d522c5b51165b55575b2b0d24a
Ziv <zivlazarov@gmail.com>
2021-07-09 14:14:56 +0300

    renamed to ChessGame

--- src/com/zivlazarov/chessengine/ui/game/ChessGame.java
+++ src/com/zivlazarov/chessengine/ui/game/ChessGame.java
@@ -2,7 +2,7 @@ package com.zivlazarov.chessengine.ui.game;
 
 import com.zivlazarov.chessengine.ui.components.BoardFrame;
 
-public class SwingGame {
+public class ChessGame {
 
     public static void main(String[] args) {
         new BoardFrame();
