7d7be04310adc5f25e321a6973483d8febfde994
ziv <zivlazarov@gmail.com>
2021-07-30 06:19:36 +0300

    added annotated classes in session factory configuration

--- src/main/java/com/zivlazarov/chessengine/db/DatabaseUtils.java
+++ src/main/java/com/zivlazarov/chessengine/db/DatabaseUtils.java
@@ -1,18 +1,34 @@
 package com.zivlazarov.chessengine.db;
 
+import com.zivlazarov.chessengine.model.board.Tile;
+import com.zivlazarov.chessengine.model.move.Move;
+import com.zivlazarov.chessengine.model.pieces.Piece;
+import com.zivlazarov.chessengine.model.player.Player;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.io.File;
+
 public class DatabaseUtils {
 
-    public static SessionFactory sessionFactory = null;
+    public static volatile SessionFactory sessionFactory = null;
+
+    public static final String DB_URL = "jdbc:mysql://localhost/chess";
+    public static final String USER = "zivlazarov";
+    public static final String PASS = "zivlazarov";
 
     public static SessionFactory createSessionFactory() {
         try {
             if (sessionFactory == null) {
                 synchronized (SessionFactory.class) {
                     if (sessionFactory == null) {
-                        sessionFactory = new Configuration().configure().buildSessionFactory();
+                        sessionFactory = new Configuration()
+                                .configure(new File("hibernate.cfg.xml"))
+                                .addAnnotatedClass(Player.class)
+                                .addAnnotatedClass(Tile.class)
+                                .addAnnotatedClass(Piece.class)
+                                .addAnnotatedClass(Move.class)
+                                .buildSessionFactory();
                         return sessionFactory;
                     }
                 }
