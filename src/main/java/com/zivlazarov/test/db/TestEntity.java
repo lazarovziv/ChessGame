1709877d4c53f89fafd0dd8330527baadb5e5fa0
ziv <zivlazarov@gmail.com>
2021-07-30 06:20:09 +0300

    added a test entity for db queries testing

---
+++ src/main/java/com/zivlazarov/test/db/TestEntity.java
@@ -0,0 +1,45 @@
+package com.zivlazarov.test.db;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "test")
+public class TestEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    public TestEntity() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
