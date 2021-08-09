b4d21cd818ecc08023f3763f028d84cb6e43f47b
Ziv <zivlazarov@gmail.com>
2021-08-09 08:51:42 +0300

    project dependencies

--- src/main/java/com/zivlazarov/chessengine/server/model/PlayerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.zivlazarov.chessengine.server.model;
-
-import com.zivlazarov.chessengine.client.model.player.Player;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PlayerRepository extends CrudRepository<Player, Integer> {
-
-}
